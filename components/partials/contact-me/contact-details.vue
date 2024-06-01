bab3ef0ff9d99ccd0a65f70001f3b31660c304f2
Amir Zuhdi Wibowo <cakadi190@gmail.com>
2024-06-01 16:35:57 +0700

    "Refactor contact details component: reformatted code, added line breaks, and updated email and social media links"

--- components/partials/contact-me/contact-details.vue
+++ components/partials/contact-me/contact-details.vue
@@ -2,7 +2,11 @@
 	<section id="contact-details">
 		<div class="mb-3">
 			<div class="alert bg-warning-subtle d-flex gap-3">
-        <Icon name="gravity-ui:triangle-exclamation-fill" class="flex-shrink-0" size="32" />
+				<Icon
+					name="gravity-ui:triangle-exclamation-fill"
+					class="flex-shrink-0"
+					size="32"
+				/>
 				<div class="content">
 					Sebagai peringatan keras! Informasi yang saya berikan dibawah ini
 					hanya untuk <strong>Kebutuhan Bisnis Saja</strong>. Selebihnya jika
@@ -24,25 +28,44 @@
 			<div class="card mb-4 overflow-hidden">
 				<div class="table-responsive">
 					<table class="table mb-0 table-padded">
-            <thead>
-              <tr>
-                <th class="w-25 text-nowrap">Kontak</th>
-                <th>Detail</th>
-              </tr>
-            </thead>
+						<thead>
+							<tr>
+								<th class="w-25 text-nowrap">Kontak</th>
+								<th>Detail</th>
+							</tr>
+						</thead>
 						<tbody>
-              <tr>
-                <td class="text-nowrap w-25">Alamat</td>
-                <td>Ngawi, Jawa Timur&nbsp;&nbsp;<span data-bs-toggle="tooltip" data-bs-title="Maaf, saya tidak bisa memberikan alamat lengkap rumah saya dengan alasan privasi. Mohon untuk menghargainya!"><Icon size="14" name="bi:info-circle-fill" /></span></td>
-              </tr>
-              <tr>
-                <td class="text-nowrap w-25">Zona Waktu</td>
-                <td>GMT+07:00 (Indonesian Western Time / UTC+7)</td>
-              </tr>
+							<tr>
+								<td class="text-nowrap w-25">Alamat</td>
+								<td>
+									Ngawi, Jawa Timur&nbsp;&nbsp;<span
+										data-bs-toggle="tooltip"
+										data-bs-title="Maaf, saya tidak bisa memberikan alamat lengkap rumah saya dengan alasan privasi. Mohon untuk menghargainya!"
+										><Icon size="14" name="bi:info-circle-fill"
+									/></span>
+								</td>
+							</tr>
+							<tr>
+								<td class="text-nowrap w-25">Zona Waktu</td>
+								<td>GMT+07:00 (Indonesian Western Time / UTC+7)</td>
+							</tr>
 							<tr>
 								<td class="w-25 text-nowrap">Surat Elektronik</td>
 								<td>
-									<a :href="`mailto:${email}`">{{ email }}</a>
+									<div class="d-flex gap-1 flex-wrap">
+										<a
+											class="text-decoration-line-through disabled"
+											:href="`mailto:${email}`"
+											>{{ email }}</a
+										>
+										<span
+											data-bs-toggle="tooltip"
+											data-bs-title="Sementara belum bisa karena hosting saya belum saya perpanjang wkwkwk."
+											><Icon size="14" name="bi:info-circle-fill"
+										/></span>
+										<span>/</span>
+										<a :href="`mailto:${altEmail}`">{{ altEmail }}</a>
+									</div>
 								</td>
 							</tr>
 							<tr>
@@ -62,45 +85,44 @@
 				</div>
 			</div>
 
-      <p>Jika Anda membutuhkan informasi lebih lanjut, seperti nomor telepon saya, jangan ragu untuk mengirimkan email kepada saya terlebih dahulu.</p>
+			<p>
+				Jika Anda membutuhkan informasi lebih lanjut, seperti nomor telepon
+				saya, jangan ragu untuk mengirimkan email kepada saya terlebih dahulu.
+			</p>
 		</div>
 
-    <div class="mt-4 border-top pt-4" id="socmed">
-      <h4 class="mb-3">Sosial Media</h4>
-
-      <div class="card mb-4 overflow-hidden">
-        <div class="table-responsive">
-          <table class="table mb-0 table-padded">
-            <thead>
-              <tr>
-                <th class="w-25 text-nowrap">Platform</th>
-                <th>Detail</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td class="w-25 text-nowrap">Facebook</td>
-                <td>
-                  <a href="https://www.facebook.com/cakadi190">cakadi190</a>
-                </td>
-              </tr>
-              <tr>
-                <td class="w-25 text-nowrap">Instagram</td>
-                <td>
-                  <a href="https://www.instagram.com/cakadi190/">cakadi190</a>
-                </td>
-              </tr>
-              <tr>
-                <td class="w-25 text-nowrap">Twitter</td>
-                <td>
-                  <a href="https://twitter.com/cakadi190">cakadi190</a>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
+		<div class="mt-4 border-top pt-4" id="socmed">
+			<h4 class="mb-3">Sosial Media</h4>
+			<p>
+				Kalau kamu mau cari semua sosial mediaku, kamu bisa menggunakan
+				pencarian <strong>@cakadi190 / @cakadi.id</strong> yang mana itu adalah
+				nama pengguna di hampir semua sosialku.
+			</p>
+			<p>
+				Tapi mungkin kamu gak punya waktu untuk nyari sosmedku, bisa deh pake
+				daftar tautan dibawah ini.
+			</p>
+			<div class="card mb-4 overflow-hidden">
+				<div class="table-responsive">
+					<table class="table mb-0 table-padded">
+						<thead>
+							<tr>
+								<th class="w-25 text-nowrap">Platform</th>
+								<th>Detail</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr v-for="link in socialMediaLinks" :key="link.platform">
+								<td class="w-25 text-nowrap">{{ link.platform }}</td>
+								<td>
+									<a :href="link.url" target="_blank" class="text-decoration-none">{{ link.url }} <Icon name="lucide:external-link" /></a>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
 	</section>
 </template>
 
@@ -111,15 +133,25 @@ export default defineComponent({
 </script>
 
 <script lang="ts" setup>
-const email: Ref<string> = ref("cakadi190@gmail.com");
+const email: Ref<string> = ref("me@cakadi.id");
+const altEmail: Ref<string> = ref("cakadi190@gmail.com");
 const phone: Ref<string> = ref("081333550746");
 const website: Ref<string> = ref("https://www.cakadi.id");
+
+const socialMediaLinks = ref([
+	{ platform: "Facebook", url: "https://www.facebook.com/cakadi190" },
+	{ platform: "Instagram", url: "https://www.instagram.com/cakadi.id/" },
+	{ platform: "Twitter", url: "https://twitter.com/cakadi190" },
+	{ platform: "Youtube", url: "https://youtube.com/@catatancakadi" },
+	{ platform: "LinkedIn", url: "https://linkedin.com/in/cakadi190" },
+]);
 </script>
 
 <style scoped lang="scss">
 .table-padded {
-  th, td {
-    padding: .625rem .875rem;
-  }
+	th,
+	td {
+		padding: 0.625rem 0.875rem;
+	}
 }
 </style>
