541772ae13bb1777fd0d6388e3e1ae82829709f6
cakadi190 <cakadi190@gmail.com>
2026-07-05 00:10:14 +0700

    chore: remove unused generated Prisma client files and update library configuration

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,8 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import pkg from "@prisma/client";
 import { Pool } from "pg";
+
+const { PrismaClient } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
