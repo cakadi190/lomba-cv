7e285a86f5a9b927cfc994f0ffc94909c9426220
cakadi190 <cakadi190@gmail.com>
2026-07-05 09:42:26 +0700

    feat: add motion animations to auth layout components and refactor about page structure

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
+import { enterSlideFromLeftToRight, initialSlideFromLeftToRight } from '~/components/motion';
 import { route } from '~~/lib/route';
 </script>
 
 <template>
   <main id="auth-wrapper">
     <div id="container">
-      <app-brand link-class="offcanvas-title" 
-          :to="route('home')" height="48" />
+      <div v-motion :enter="enterSlideFromLeftToRight" :initial="initialSlideFromLeftToRight">
+        <app-brand link-class="offcanvas-title" :to="route('home')" height="48" />
+      </div>
       <div class="card p-4 rounded-4 card-body w-100">
         <slot />
       </div>
       <div class="mx-auto d-flex flex-column justify-content-center pt-2">
-        <div class="mb-2 text-center">&copy; 2024 Catatan Cak Adi</div>
-        <div class="mx-auto">
+        <div v-motion :enter="enterSlideFromLeftToRight" :initial="initialSlideFromLeftToRight"
+          class="mb-2 text-center">&copy; 2024 Catatan Cak Adi</div>
+        <div v-motion :enter="enterSlideFromLeftToRight" :initial="initialSlideFromLeftToRight" class="mx-auto">
           <theme-toggler />
         </div>
       </div>
@@ -49,8 +52,8 @@ import { route } from '~~/lib/route';
     z-index: 2;
     width: calc(100% * (1/4));
     margin-block: auto;
-    
-    @media (width <= 992px) {
+
+    @media (width <=992px) {
       max-width: 100%;
     }
   }
--- app/pages/tentang/saya.vue
+++ app/pages/tentang/saya.vue
@@ -1,238 +1,186 @@
 <template>
-	<div id="about-me">
-		<header-page>
-			<template #title>Tentang Saya</template>
-			<template #subtitle>Berikut sekilas tentang diri saya pribadi</template>
-		</header-page>
+  <div id="about-me">
+    <header-page>
+      <template #title>Tentang Saya</template>
+      <template #subtitle>Berikut sekilas tentang diri saya pribadi</template>
+    </header-page>
 
-		<section class="need-space pt-0">
-			<div class="container">
-				<div class="row flex-column-reverse flex-md-row gy-5">
-					<div class="col-md-9">
-						<!-- About Me -->
-						<div id="about-me">
-							<div class="row gy-3 pb-5">
-								<div class="col-md-4">
-									<div
-										v-motion
-										:enter="enterSlideFromBottomToTop"
-										:initial="initialSlideFromBottomToTop"
-										class="card card-body rounded-4 p-4 text-center align-items-center"
-									>
-										<Icon name="fa6-solid:medal" size="48" />
-										<strong class="h3 mb-0 pt-3"
-											>{{ countCareerStart }} Tahun</strong
-										>
-										<span>Berpengalaman</span>
-									</div>
-								</div>
-								<div class="col-md-4">
-									<div
-										v-motion
-										:enter="enterSlideFromBottomToTop"
-										:initial="initialSlideFromBottomToTop"
-										class="card card-body rounded-4 p-4 text-center align-items-center"
-									>
-										<Icon name="fa6-solid:handshake" size="48" />
-										<strong class="h3 mb-0 pt-3"
-											>{{ countCareerDev }} Tahun</strong
-										>
-										<span>Melayani</span>
-									</div>
-								</div>
-								<div class="col-md-4">
-									<nuxt-link
-										to="/portofolio"
-										v-motion
-										:enter="enterSlideFromBottomToTop"
-										:initial="initialSlideFromBottomToTop"
-										class="card card-body rounded-4 p-4 text-center align-items-center text-decoration-none"
-									>
-										<Icon name="fa6-solid:briefcase" size="48" />
-										<strong class="h3 mb-0 pt-3">{{ totalProjects ?? 0 }}</strong>
-										<span>Proyek</span>
-									</nuxt-link>
-								</div>
-							</div>
+    <section class="need-space pt-0">
+      <div class="container">
+        <div class="row flex-column-reverse flex-md-row gy-5">
+          <div class="col-md-9">
+            <!-- About Me -->
+            <div id="about-me">
+              <div class="row gy-3 pb-5">
+                <div class="col-md-4">
+                  <div v-motion :enter="enterSlideFromBottomToTop" :initial="initialSlideFromBottomToTop"
+                    class="card card-body rounded-4 p-4 text-center align-items-center">
+                    <Icon name="fa6-solid:medal" size="48" />
+                    <strong class="h3 mb-0 pt-3">{{ countCareerStart }} Tahun</strong>
+                    <span>Berpengalaman</span>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <div v-motion :enter="enterSlideFromBottomToTop" :initial="initialSlideFromBottomToTop"
+                    class="card card-body rounded-4 p-4 text-center align-items-center">
+                    <Icon name="fa6-solid:handshake" size="48" />
+                    <strong class="h3 mb-0 pt-3">{{ countCareerDev }} Tahun</strong>
+                    <span>Melayani</span>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <nuxt-link to="/portofolio" v-motion :enter="enterSlideFromBottomToTop"
+                    :initial="initialSlideFromBottomToTop"
+                    class="card card-body rounded-4 p-4 text-center align-items-center text-decoration-none">
+                    <Icon name="fa6-solid:briefcase" size="48" />
+                    <strong class="h3 mb-0 pt-3">{{ totalProjects ?? 0 }}</strong>
+                    <span>Proyek</span>
+                  </nuxt-link>
+                </div>
+              </div>
 
-							<div class="mb-5 pb-5 border-bottom">
-								<div class="row gy-4">
-									<div class="col-md-5">
-										<nuxt-img
-											preload
-											src="/images/programmer.svg"
-											v-motion
-											:enter="enterSlideFromLeftToRight"
-											:initial="initialSlideFromLeftToRight"
-											class="w-100"
-										/>
-									</div>
-									<div
-										class="col-md-7"
-										v-motion
-										:enter="enterSlideFromRightToLeft"
-										:initial="initialSlideFromRightToLeft"
-									>
-										<p>
-											Hai, saya <strong>Amir Zuhdi Wibowo, A.Md.Kom.</strong>, seorang
-											<strong>Software Engineer</strong> dan <strong>Wirausahawan</strong> yang hobi ngulik
-											dari front-end sampai back-end. Saya senang membuat
-											aplikasi web yang nggak cuma berfungsi dengan baik tapi
-											juga enak dilihat. Dari HTML, CSS, JavaScript, sampai PHP,
-											saya punya pengalaman yang luas untuk memastikan proyek
-											web berjalan lancar dan terlihat menarik.
-										</p>
-										<p>
-											Selain coding, saya juga jago di beberapa bidang IT
-											lainnya. Saya bisa bikin desain grafis yang kece dengan
-											Photoshop dan Illustrator, merancang UI/UX yang
-											user-friendly pakai Figma, dan bahkan reparasi komputer
-											ringan kalau ada masalah. Kombinasi keterampilan ini bikin
-											saya siap banget untuk memberikan yang terbaik di setiap
-											proyek yang saya kerjakan.
-										</p>
-									</div>
-								</div>
-							</div>
-						</div>
+              <div class="mb-5 pb-5 border-bottom">
+                <div class="row gy-4">
+                  <div class="col-md-5">
+                    <nuxt-img preload src="/images/programmer.svg" v-motion :enter="enterSlideFromLeftToRight"
+                      :initial="initialSlideFromLeftToRight" class="w-100" />
+                  </div>
+                  <div class="col-md-7" v-motion :enter="enterSlideFromRightToLeft"
+                    :initial="initialSlideFromRightToLeft">
+                    <p>
+                      Hai, saya <strong>Amir Zuhdi Wibowo, A.Md.Kom.</strong>, seorang
+                      <strong>Software Engineer</strong> dan <strong>Wirausahawan</strong> yang hobi ngulik
+                      dari front-end sampai back-end. Saya senang membuat
+                      aplikasi web yang nggak cuma berfungsi dengan baik tapi
+                      juga enak dilihat. Dari HTML, CSS, JavaScript, sampai PHP,
+                      saya punya pengalaman yang luas untuk memastikan proyek
+                      web berjalan lancar dan terlihat menarik.
+                    </p>
+                    <p>
+                      Selain coding, saya juga jago di beberapa bidang IT
+                      lainnya. Saya bisa bikin desain grafis yang kece dengan
+                      Photoshop dan Illustrator, merancang UI/UX yang
+                      user-friendly pakai Figma, dan bahkan reparasi komputer
+                      ringan kalau ada masalah. Kombinasi keterampilan ini bikin
+                      saya siap banget untuk memberikan yang terbaik di setiap
+                      proyek yang saya kerjakan.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
 
-						<!-- My History -->
-						<div id="history" class="pb-4 mb-4 border-bottom">
-							<h4>Sejarah Singkat</h4>
+            <!-- My History -->
+            <div id="history" class="pb-4 mb-4 border-bottom">
+              <h4>Sejarah Singkat</h4>
 
-							<p>
-								Perjalanan saya sebagai programmer dimulai secara tidak sengaja
-								pada pertengahan 2015 saat mengerjakan tugas PKn. Awalnya
-								berkenalan dengan
-								<a href="https://blogger.com" rel="noreferrer" target="_blank"
-									>Blogger</a
-								>, lalu beralih ke WordPress, yang memperkenalkan saya pada
-								konsep CMS yang lebih kompleks. Langkah berikutnya membawa saya
-								ke dunia hosting dan manajemen server, di mana saya mempelajari
-								panel kontrol seperti cPanel dan aspek teknis di balik sebuah
-								website.
-							</p>
+              <p>
+                Perjalanan saya sebagai programmer dimulai secara tidak sengaja
+                pada pertengahan 2015 saat mengerjakan tugas PKn. Awalnya
+                berkenalan dengan
+                <a href="https://blogger.com" rel="noreferrer" target="_blank">Blogger</a>, lalu beralih ke WordPress,
+                yang
+                memperkenalkan saya pada
+                konsep CMS yang lebih kompleks. Langkah berikutnya membawa saya
+                ke dunia hosting dan manajemen server, di mana saya mempelajari
+                panel kontrol seperti cPanel dan aspek teknis di balik sebuah
+                website.
+              </p>
 
-							<p>
-								Perjalanan ini berlanjut dengan mempelajari dasar-dasar HTML,
-								CSS, dan JavaScript untuk frontend, serta PHP dan MySQL untuk
-								backend. Saya menghadapi berbagai tantangan, dari error yang
-								membingungkan hingga konsep-konsep sulit. Seiring waktu, saya
-								mengeksplorasi teknologi modern seperti Node.js dan React,
-								memperluas pemahaman saya tentang ekosistem pengembangan web
-								fullstack.
-							</p>
+              <p>
+                Perjalanan ini berlanjut dengan mempelajari dasar-dasar HTML,
+                CSS, dan JavaScript untuk frontend, serta PHP dan MySQL untuk
+                backend. Saya menghadapi berbagai tantangan, dari error yang
+                membingungkan hingga konsep-konsep sulit. Seiring waktu, saya
+                mengeksplorasi teknologi modern seperti Node.js dan React,
+                memperluas pemahaman saya tentang ekosistem pengembangan web
+                fullstack.
+              </p>
 
-							<p>
-								Menjadi fullstack developer adalah perjalanan penuh liku. Ada
-								saat-saat frustrasi ketika menghadapi bug atau konfigurasi
-								server yang kompleks, namun juga ada momen membahagiakan saat
-								berhasil mengoptimalkan website atau mengimplementasikan fitur
-								challenging. Kini, setelah bertahun-tahun, saya bersyukur atas
-								perjalanan tidak disengaja ini yang telah membentuk karir dan
-								passion saya dalam dunia pengembangan web.
-							</p>
-						</div>
+              <p>
+                Menjadi fullstack developer adalah perjalanan penuh liku. Ada
+                saat-saat frustrasi ketika menghadapi bug atau konfigurasi
+                server yang kompleks, namun juga ada momen membahagiakan saat
+                berhasil mengoptimalkan website atau mengimplementasikan fitur
+                challenging. Kini, setelah bertahun-tahun, saya bersyukur atas
+                perjalanan tidak disengaja ini yang telah membentuk karir dan
+                passion saya dalam dunia pengembangan web.
+              </p>
+            </div>
 
-						<!-- Skills -->
-						<div id="skills" class="pb-5 mb-5 border-bottom">
-							<h4>Keahlian Saya</h4>
-							<p class="opacity-75 mb-5">Keahlian profesional saya.</p>
+            <!-- Skills -->
+            <div id="skills" class="pb-5 mb-5 border-bottom">
+              <h4>Keahlian Saya</h4>
+              <p class="opacity-75 mb-5">Keahlian profesional saya.</p>
 
-							<div class="skill-main-wrapper">
-								<div
-									class="skill"
-									v-for="(item, index) in skillset"
-									:key="index"
-								>
-									<Icon :name="item.icon" />
-									<p class="m-0">{{ item.name }}</p>
-								</div>
-							</div>
-						</div>
+              <div class="skill-main-wrapper">
+                <div class="skill" v-for="(item, index) in skillset" :key="index">
+                  <Icon :name="item.icon" />
+                  <p class="m-0">{{ item.name }}</p>
+                </div>
+              </div>
+            </div>
 
-						<!-- Certification -->
-						<div id="certification">
-							<h4>Sertifikasi Saya</h4>
-							<p class="opacity-75 mb-4">
-								Beberapa sertifikat yang sudah dan pernah saya dapatkan.
-							</p>
+            <!-- Certification -->
+            <div id="certification">
+              <h4>Sertifikasi Saya</h4>
+              <p class="opacity-75 mb-4">
+                Beberapa sertifikat yang sudah dan pernah saya dapatkan.
+              </p>
 
-							<ClientOnly>
-								<swiper-container
-									:modules="[SwiperAutoplay, SwiperEffectCreative]"
-									:slides-per-view="2"
-									:loop="true"
-									:space-between="16"
-								>
-									<swiper-slide v-for="slide in 10" :key="slide">
-										<div
-											style="background-color: rgba(255, 255, 255, 0.1)"
-											class="ratio ratio-16x9 rounded-4"
-										></div>
-									</swiper-slide>
-								</swiper-container>
-							</ClientOnly>
-						</div>
-					</div>
-					<div class="col-md-3">
-						<div
-							class="card sticky-top overflow-hidden rounded-4"
-							v-motion
-							:enter="enterSlideFromBottomToTop"
-							:initial="initialSlideFromBottomToTop"
-						>
-							<div class="card-header p-4">
-								<h4 class="mb-0">Navigasi</h4>
-							</div>
-							<div class="card-body">
-								<!-- Tab panes Title -->
-								<ul
-									class="nav nav-pills flex-column align-items-stretch"
-									v-motion
-									:enter="enterSlideFromBottomToTop"
-									:initial="initialSlideFromBottomToTop"
-									id="myTab"
-								>
-									<li class="nav-item">
-										<a href="#about-me" class="nav-link py-3 w-100 text-start">
-											Tentang Saya
-										</a>
-									</li>
-									<li class="nav-item">
-										<a href="#history" class="nav-link py-3 w-100 text-start">
-											Sejarah Singkat Saya
-										</a>
-									</li>
-									<li class="nav-item">
-										<a href="#skills" class="nav-link py-3 w-100 text-start">
-											Keahlian
-										</a>
-									</li>
-									<li class="nav-item">
-										<a
-											href="#certification"
-											class="nav-link py-3 w-100 text-start"
-										>
-											Sertifikasi
-										</a>
-									</li>
-									<li class="nav-item">
-										<nuxt-link
-											to="/tentang/skill"
-											class="nav-link py-3 w-100 text-start"
-										>
-											Perkakas
-										</nuxt-link>
-									</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	</div>
+              <ClientOnly>
+                <swiper-container :modules="[SwiperAutoplay, SwiperEffectCreative]" :slides-per-view="2" :loop="true"
+                  :space-between="16">
+                  <swiper-slide v-for="slide in 10" :key="slide">
+                    <div style="background-color: rgba(255, 255, 255, 0.1)" class="ratio ratio-16x9 rounded-4"></div>
+                  </swiper-slide>
+                </swiper-container>
+              </ClientOnly>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card sticky-top overflow-hidden rounded-4" v-motion :enter="enterSlideFromBottomToTop"
+              :initial="initialSlideFromBottomToTop">
+              <div class="card-header p-4">
+                <h4 class="mb-0">Navigasi</h4>
+              </div>
+              <div class="card-body">
+                <!-- Tab panes Title -->
+                <ul class="nav nav-pills flex-column align-items-stretch" v-motion :enter="enterSlideFromBottomToTop"
+                  :initial="initialSlideFromBottomToTop" id="myTab">
+                  <li class="nav-item">
+                    <a href="#about-me" class="nav-link py-3 w-100 text-start">
+                      Tentang Saya
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#history" class="nav-link py-3 w-100 text-start">
+                      Sejarah Singkat Saya
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#skills" class="nav-link py-3 w-100 text-start">
+                      Keahlian
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#certification" class="nav-link py-3 w-100 text-start">
+                      Sertifikasi
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <nuxt-link to="/tentang/skill" class="nav-link py-3 w-100 text-start">
+                      Perkakas
+                    </nuxt-link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -312,7 +260,8 @@ const skillset = ref([
   { icon: "devicon:vuejs", name: "VueJS" },
   { icon: "devicon:bootstrap", name: "Bootstrap" },
   { icon: "devicon:tailwindcss", name: "TailwindCSS" },
-  { icon: "devicon:angular", name: "AngularJS" },
+  { icon: "devicon:react", name: "ReactJS" },
+  { icon: "devicon:nextjs", name: "NextJS" },
   { icon: "devicon:jquery", name: "jQuery" },
   { icon: "devicon:typescript", name: "Typescript" },
   { icon: "devicon:git", name: "Git" },
@@ -331,80 +280,80 @@ useHead({
 
 <style lang="scss">
 .card {
-	&.sticky-top {
-		position: sticky;
-		top: 7.5rem;
-		z-index: 1020;
-	}
+  &.sticky-top {
+    position: sticky;
+    top: 7.5rem;
+    z-index: 1020;
+  }
 }
 
 @keyframes scroll {
-	0% {
-		transform: translateX(100%);
-	}
+  0% {
+    transform: translateX(100%);
+  }
 
-	to {
-		transform: translateX(-100%);
-	}
+  to {
+    transform: translateX(-100%);
+  }
 }
 
 .skill-main-wrapper {
-	display: flex;
-	gap: 0.75rem;
-	flex-direction: row;
-	flex-wrap: wrap;
-	width: 100%;
+  display: flex;
+  gap: 0.75rem;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
 
-	.skill {
-		padding: 0.75rem 1.5rem;
-		border-radius: 0.5rem;
-		display: flex;
-		flex-wrap: wrap;
-		font-size: 1.25em;
-		align-items: center;
-		gap: 1rem;
-		white-space: nowrap;
-		background: rgba(var(--bs-body-color-rgb), 0.125);
-		transition: outline 0.5s all;
-		position: relative;
-		z-index: 0;
-		cursor: pointer;
-		transition: all 0.2s;
-		border-color: 1px solid transparent;
+  .skill {
+    padding: 0.75rem 1.5rem;
+    border-radius: 0.5rem;
+    display: flex;
+    flex-wrap: wrap;
+    font-size: 1.25em;
+    align-items: center;
+    gap: 1rem;
+    white-space: nowrap;
+    background: rgba(var(--bs-body-color-rgb), 0.125);
+    transition: outline 0.5s all;
+    position: relative;
+    z-index: 0;
+    cursor: pointer;
+    transition: all 0.2s;
+    border-color: 1px solid transparent;
 
-		&:hover {
-			transform: scale(1.05);
-			border-color: var(--bs-primary);
-		}
-	}
+    &:hover {
+      transform: scale(1.05);
+      border-color: var(--bs-primary);
+    }
+  }
 }
 </style>
 
 <style scoped lang="scss">
 .nav-pills {
-	.nav-item {
-		.nav-link {
-			color: var(--bs-body-color);
-			opacity: 0.75;
+  .nav-item {
+    .nav-link {
+      color: var(--bs-body-color);
+      opacity: 0.75;
 
-			&.active {
-				background-color: transparent;
-				box-shadow: none;
-				opacity: 1;
-			}
-		}
+      &.active {
+        background-color: transparent;
+        box-shadow: none;
+        opacity: 1;
+      }
+    }
 
-		&:not(:last-child) {
-			border-bottom: 1px solid var(--bs-card-border-color);
-		}
-	}
+    &:not(:last-child) {
+      border-bottom: 1px solid var(--bs-card-border-color);
+    }
+  }
 }
 
 .card {
-	&.sticky-top {
-		position: sticky;
-		top: 7.5rem;
-		z-index: 1020;
-	}
+  &.sticky-top {
+    position: sticky;
+    top: 7.5rem;
+    z-index: 1020;
+  }
 }
 </style>
