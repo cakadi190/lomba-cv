2b8b822145a086e81b76f21d9b3628b1df9975f9
Amir Zuhdi Wibowo, A.Md.Kom. <cakadi190@gmail.com>
2026-09-15 00:26:32 +0700

    fix: replace nuxt-img with img to resolve rendering issues

--- app/components/error-section.vue
+++ app/components/error-section.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="text-center">
-		<nuxt-img preload class="mb-4" :src="imgSrc" :alt="imgAlt" :height="imgHeight" />
+		<img preload class="mb-4" :src="imgSrc" :alt="imgAlt" :height="imgHeight" />
 		<h3>{{ title }}</h3>
 		<p class="text-muted">{{ text }}</p>
 	</div>
