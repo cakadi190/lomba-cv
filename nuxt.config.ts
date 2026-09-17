ca7bd60cf9b649f622afb7238983bc9d9bf9c356
Amir Zuhdi Wibowo, A.Md.Kom. <cakadi190@gmail.com>
2026-09-18 05:01:27 +0700

    fix: rename NUXT_TURNSTILE_SITE_KEY to NUXT_PUBLIC_TURNSTILE_SITE_KEY for clarity

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,7 +34,7 @@ export default defineNuxtConfig({
   },
 
   turnstile: {
-    siteKey: process.env.NUXT_TURNSTILE_SITE_KEY || "",
+    siteKey: process.env.NUXT_PUBLIC_TURNSTILE_SITE_KEY || "",
     addValidateEndpoint: true,
   },
 
